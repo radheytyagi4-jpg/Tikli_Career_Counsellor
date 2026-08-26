7cd492889fe9bb62f2f27e311b7bd6e5c4a78034
radhey <radheytyagi4@gmail.com>
2026-08-26 12:06:46 +0530

    Fix Express 5 wildcard route syntax

--- backend/app.js
+++ backend/app.js
@@ -24,7 +24,7 @@ app.use('/api/v2/user', router);
 // frontend
 app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-app.get("*", (req, res) => {
+app.get("*splat", (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend/dist", "index.html"));
 });
 
